10105b9872155a730bef10eeb6413822afd3f873
Olha Stratii <shmelova83@gmail.com>
2016-06-30 16:52:22 +0300

    initial commit

---
+++ js/src/calc.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+    var outputField = $(".outputField"),
+        inputElement = $(".operationsField span, .numbersField span"),
+        resultKey = $(".result span"),
+        result = 0,
+        calc = new Calculator();
+
+    inputElement.click(function () {
+        var currentValue = this.textContent;
+
+        outputField.append(currentValue);
+    });
+
+    resultKey.click(function () {
+        var insertedExpression = outputField[0].innerHTML;
+        if (calc.parse(insertedExpression)) {
+            outputField.text(calc.process());
+        }
+    });
+
+});
+
+function Calculator() {
+    this.parse = function (expression) {
+        this.expression = expression;
+        return true;
+    };
+    this.process = function () {
+        return eval(this.expression);
+    };
+}
