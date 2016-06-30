97547a24deb89337073d0fb9013e4aff9ce493b6
Olha Stratii <shmelova83@gmail.com>
2016-06-30 16:52:58 +0300

    spec added

---
+++ js/spec/calcSpec.js
@@ -0,0 +1,28 @@
+describe("Calculator", function () {
+    var calc;
+
+    beforeEach(function () {
+        calc = new Calculator();
+    });
+
+    describe("when calculator created", function () {
+
+
+        it("should be able to check an expression", function () {
+            expect(calc.parse("76/2+7*6")).toBe(true);
+            expect(calc.parse("*1")).toBe(false);
+            expect(calc.parse("--2+6")).toBe(false);
+            expect(calc.parse("-2+6")).toBe(true);
+            expect(calc.parse(".3")).toBe(false);
+            expect(calc.parse("34..6")).toBe(false);
+            expect(calc.parse("34.6")).toBe(true);
+        });
+
+        it("should be able to process correct expression", function () {
+            var expression = "2+3/2*10";
+                expect(calc.process()).toEqual(17);
+        });
+
+    });
+
+});
